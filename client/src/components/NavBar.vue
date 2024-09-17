75b0c742ef8eee79bd51307b762e877648e7e831
Niki <nikip9795@gmail.com>
2024-09-16 20:43:45 -0400

    Nav Bar in-progress

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,68 +1,56 @@
-<!-- components/Navbar.vue -->
-
 <template>
   <header class="leading-relaxed max-h-screen">
     <nav>
-      <div class="mx-auto p-4 font-normal">
-        <div class="no-underline text-blueviolet">
+      <div class="mx-auto p-3 font-normal">
+        <div class="flex items-center justify-between w-full text-blueviolet">
+          <div class="flex items-center space-x-6">
 
-          <RouterLink 
-            to="/" 
-            class="transition duration-400 
-                   hover:bg-[hsla(281,26%,65%,0.2)]
-                   mr-2">
-            Home
-          </RouterLink>
+            <RouterLink to="/">
+              <img src="../assets/user.jpeg" class="h-12 w-12"/>
+            </RouterLink>
+            
+            <RouterLink 
+              to="/" 
+              class="transition duration-500 
+                    hover:bg-[hsla(281,26%,65%,0.2)]
+                    font-semibold
+                    p-1 rounded-md">
+              Dashboard
+            </RouterLink>
+          </div>
 
-          <RouterLink 
-            to="/log in" 
-            class="transition duration-400 
-                   hover:bg-[hsla(281,26%,65%,0.2)]
-                   mr-2">
-            Log in
-          </RouterLink>
-
-          <RouterLink 
-            to="/repos" 
-            class="transition duration-400 
-                   hover:bg-[hsla(281,26%,65%,0.2)]
-                   mr-2">
-            Repos
-          </RouterLink>
-
-          <RouterLink 
-            to="/profile" 
-            class="transition duration-400 
-                   hover:bg-[hsla(281,26%,65%,0.2)]
-                   mr-2">
-            Profile
-          </RouterLink>
-
-          <RouterLink 
-            to="/githubrepos" 
-            class="transition duration-400
-                   hover:bg-[hsla(281,26%,65%,0.2)]
-                   mr-2">
-            GitHub API Repos
-          </RouterLink>
-
-          <RouterLink 
-            to="/githubgists" 
-            class="transition duration-400
-                   hover:bg-[hsla(281,26%,65%,0.2)]
-                   mr-2">
-            GitHub API Gist
-          </RouterLink>
-
+          <div v-if="userData">
+            <img :src="userData.avatar_url" class="h-8 w-8 rounded-full cursor-pointer" />
+          </div>
+          
         </div>
       </div>
     </nav>
     <hr/>
   </header>
-
   <RouterView />
 </template>
   
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
+import { onMounted, ref } from 'vue';
+import type { GithubUser} from '../APIsInterface'
+import axios from 'axios';
+
+let userData = ref<GithubUser>()
+
+onMounted(async () => {
+  console.log("Page 1 mounted")
+
+  let userURL = 'http://localhost:5087/ghsecure/user'
+
+  // Use axios to load the user data - read up on async/await to make
+  // asynchronous calls easier
+  let userAPI = await axios.get<GithubUser>(userURL)
+
+  // If the request is successful (status 200), set the userData value
+  if (userAPI.status === 200) {
+    userData.value = userAPI.data
+  }
+})
 </script>
